4eb6ff6aac455d8d1978a78914004e5cba0c6c8a
UnixUser1971 <86995387+UnixUser1971@users.noreply.github.com>
2021-08-31 19:53:07 -0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+print ("In a world coveted in darkness the seas RAGE with power the tree bristle in the wind as though it was a sword to be \nunsheathed. YOU have been brought here to stop the evil curse put upon these lands!")
+
+print("\nPlease input Username *note you will not be able to change your name later")
+
+name = input()
+
+print ("\nIs this correct?")
+print (name)
+
+pick = input("\nPlease input y or n")
+print(" ")
+if pick=="y":
+    print("let the story BEGIN!")
+elif pick=="n":    
+    print("Please reinput Username!\n")
+    name = input()
+else:
+    print("Answer Must Be 'y' or 'n'")
+    print("\nPlease restart")
+
+print("You find yourself in darkhaven a small village in an endless winter. Pines scattered everywere. Most of the houses have damaged supports making many houses look like they may gonna topple over.")
+
+print "you may either \n1. conversate with the villagers that reside \n2.  "
+
+
+
+
+
+
+
+
+
