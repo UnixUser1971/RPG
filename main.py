6aa93a7dd832fc9c24b890db5857c2f04a8efc5f
UnixUser1971 <86995387+UnixUser1971@users.noreply.github.com>
2021-08-31 19:56:26 -0600

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-print ("In a world coveted in darkness the seas RAGE with power the tree bristle in the wind as though it was a sword to be \nunsheathed. YOU have been brought here to stop the evil curse put upon these lands!")
+print ("In a world enveloped in darkness the seas RAGE with power the tree bristle in the wind as though it was a sword to be \nunsheathed. YOU have been brought here to stop the evil curse put upon these lands!")
 
 print("\nPlease input Username *note you will not be able to change your name later")
 
